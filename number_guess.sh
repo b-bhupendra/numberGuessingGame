be62970487c9d56fefa15aa7fa2425546ae34238
b-bhupendra <100297227+b-bhupendra@users.noreply.github.com>
2022-06-25 20:01:49 +0530

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,113 +1,87 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+#############################################
+#SECRET
+GUESS_IT=$((1+RANDOM%1000));
+##############################################
+ 
+#####################################
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c";
 
-COMPARE(){
+#####################################
 
- # 2 will be the STDIN and 1 will be the secret
+###############################################
+#       MAIN FUNCTION
+#############################################
+echo "Enter your username:";
+read username;
 
-  if [ $1 -gt $2 ]
-  then
-    echo -e "\nIt's higher than that, guess again:" 
-  else
-    echo -e "\nIt's lower than that, guess again:" 
-  fi
+USER_DATA=$($PSQL "SELECT * FROM users WHERE username='$username';");
 
-}
-
-UPDATE_DB(){
-# GAME_PLAYED OLD $2
-GP=$(($2+1))
-LG=$4
-# if lowest guess / best score is 0 tie not played 
-if [[ $3 -ne 0 ]]
-then
-  if [[ $4 -ge $3 ]]
-  then
-    LG=$3
-  fi
-fi
-
-($PSQL "update user_info set  game_played = $GP ,lowest_guess = $LG where username = '$USERNAME';") >> /dev/null 
-}
-
-GUESS_GAME(){
-
-# TBG
-SECRET=$((1 + $RANDOM % 1000))
-
-echo -e "THE SECRET TESTING: $SECRET"
-
-echo -e "\nGuess the secret number between 1 and 1000:"
-
-TRY=1
-###############################
-read VAL
-
-####################
-while [[ ! $VAL =~ ^[0-9]+$ ]]
-do
- echo -e "\nThat is not an integer, guess again:"
- read VAL
-done
-####################
-if [[ $SECRET -ne $VAL ]]
+if [[ $USER_DATA ]]
 then
 
-    COMPARE $SECRET $VAL
-    ###############################
-    while [ $VAL -ne $SECRET ]
-    do
-    ((TRY=TRY+1))
-    ###############################
-    read VAL
+IFS="|"; read username games_played best_game <<< $USER_DATA
 
-    ####################
-    while [[ ! $VAL =~ ^[0-9]+$ ]]
-    do
-    echo -e "\nThat is not an integer, guess again:"
-    read VAL
-    done
-    ####################
-
-    COMPARE $SECRET $VAL
-    ###############################
-    done
-fi
-UPDATE_DB $1 $2 $3 $TRY
-echo -e "You guessed it in $TRY tries. The secret number was $SECRET. Nice job!"
-}
-
-
-
-MAIN(){
-
-echo -e "\nEnter your username:"
-
-# ask for username 
-read USERNAME 
-
-# get data for this particular username
-USER_NAME=$($PSQL "select username from user_info where username='$USERNAME'")
-GAME_PLAYED=$($PSQL "select game_played from user_info where username='$USERNAME'")
-LOWEST_GUESS=$($PSQL "select lowest_guess from user_info where username='$USERNAME'")
-
-#if users data is null then
-if [[ -z $USER_NAME ]]
-then
-
-  ($PSQL "insert into user_info values('$USERNAME');") >> /dev/null
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-  GUESS_GAME $USERNAME 0 0
+echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
 
 else
 
-  echo -e "\nWelcome back, $USER_NAME! You have played $GAME_PLAYED games, and your best game took $LOWEST_GUESS guesses."    
-  GUESS_GAME $USER_NAME $GAME_PLAYED $LOWEST_GUESS
+($PSQL "INSERT INTO users VALUES('$username')") >> /dev/null
+
+echo "Welcome, $username! It looks like this is your first time here."
 
 fi
 
-}
+################################################
+
+################################################
+
+################################################
+#             NUMBER GUSSING GAME
+################################################
 
 
-MAIN
+echo "Guess the secret number between 1 and 1000:"
+read  USER_INPUT
+
+TRY=1 # try with 0
+
+while [[ $USER_INPUT -ne $GUESS_IT ]]
+do
+
+    ##############################
+    # ensure that input is a number
+    ##############################
+
+    while [[ ! $USER_INPUT =~ ^[0-9]+$ ]]
+    do
+      echo "That is not an integer, guess again:"
+      read USER_INPUT
+    done
+
+
+    #####################################
+    # Assist user with high/low guess hint
+    ######################################
+
+    if [[ $USER_INPUT -gt $GUESS_IT ]]
+    then
+      echo "It's lower than that, guess again:"
+    else
+      echo  "It's higher than that, guess again:"
+    fi
+
+    read USER_INPUT
+    ((TRY=TRY+1))
+
+done
+
+################################################
+#       NUMBER GUSSING GAME END
+################################################
+echo "You guessed it in $TRY tries. The secret number was $GUESS_IT. Nice job!"
+
+########### UPDATION QUERY ###############
+X=$(psql --username=freecodecamp --dbname=number_guess -t --no-align -c "update users set no_of_games=no_of_games+1 where username='$username'")
+X=$(psql --username=freecodecamp --dbname=number_guess -t --no-align -c "update users set best_gaame=$TRY where username='$username' AND (best_gaame>$TRY)")
